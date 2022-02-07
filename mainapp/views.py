cf74d681956a29b0bbddaef59090d11d0a030287
Karl <bi-ckei-666@mail.ru>
2022-02-08 00:38:55 +0300

    change_view

--- mainapp/views.py
+++ mainapp/views.py
@@ -66,7 +66,7 @@ class BaseView(CartMixin, View):
         )
 
        
-        paginator = Paginator(products, 4)
+        paginator = Paginator(products, 8)
         page_number = self.request.GET.get('page')
         product_list = paginator.get_page(page_number)
         context = {
